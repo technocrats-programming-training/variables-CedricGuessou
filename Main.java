81deada59a55cb2bbc780953e8e6e34c386ea4c0
CedricGuessou <121789165+CedricGuessou@users.noreply.github.com>
2023-06-16 00:32:43 +0000

    Changed the program to show the desired output

--- Main.java
+++ Main.java
@@ -12,12 +12,18 @@ public class Main {
         
 
         // Print out the following string: "Hello, I am <firstName> <lastName>, and I am <age> years old"
+      System.out.println("Hello, I am " + firstName + " " + lastName + ", and I am " + age + " years old");
 
         // Print out whether age is even
+      boolean evenAge = age % 2==0;
+        System.out.println("My age is even: " + evenAge);
 
         // Print out whether firstName equals lastName
+      System.out.println("I have the same first and last name: " + firstName.equals(lastName));
 
         // Print out whether age is greater than 0
+      boolean ageAbove0 = age>= 0;
+      System.out.println("My age is greater than 0: " + ageAbove0);
 
     }
 }
